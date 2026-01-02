86fbd464518615647d45bb871e2a0cf61e21aa52
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2026-01-02 12:33:40 +0200

    Happy New Year!

--- plugins/system/panopticon/polyfills.php
+++ plugins/system/panopticon/polyfills.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2026 Nikolaos Dionysopoulos / Akeeba Ltd
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
+
 /**
  * Polyfills for PHP 8 features on PHP 7 servers.
  *
